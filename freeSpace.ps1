29eb7f47ab63c2c0759a3e60076d1d303df85375
Kris Clark-Berroth <krisclarkberroth@gmail.com>
2020-01-14 15:42:29 -0600

    changes

---
+++ freeSpace.ps1
@@ -0,0 +1,7 @@
+ $freeSpace = (Get-Volume -DriveLetter C).SizeRemaining/1GB
+
+if ($freeSpace -lt 2){
+    Write-EventLog -LogName Application -Source Application -EventID 0 -EntryType Warning -Message "Insufficient disk space. Habitat not installed."
+    Write-Host "Insufficient disk spack on C disk. Habitat not installed."
+    Exit
+} 
