4bb5d35b550baa51e21af4447765ed9761fbfa0d
Clark-Berroth <kris.clark-berroth@aig.com>
2020-01-14 15:48:45 -0600

    changes

--- freeSpace.ps1
+++ freeSpace.ps1
@@ -1,4 +1,4 @@
- $freeSpace = (Get-Volume -DriveLetter C).SizeRemaining/1GB
+$freeSpace = (Get-Volume -DriveLetter C).SizeRemaining/1GB
 
 if ($freeSpace -lt 2){
     Write-EventLog -LogName Application -Source Application -EventID 0 -EntryType Warning -Message "Insufficient disk space. Habitat not installed."
